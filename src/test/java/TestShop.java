113833b0fe92388fe952c094f4d9664ef4a696c3
Anastasiya <soidberg91@gmail.com>
2023-03-12 14:35:49 +0300

    1

---
+++ src/test/java/TestShop.java
@@ -0,0 +1,29 @@
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestShop {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+
+
+    }
+
+    @Test
+    void testShopAuto() {
+
+        open("http://intershop.skillbox.ru/");
+        $(".site-title").shouldBe(Condition.visible);
+        $("[name=s]").setValue("Машина").pressEnter();
+        $$("li.shipping-taxable").first().shouldBe(Condition.visible)
+                .shouldHave(Condition.textCaseSensitive("машина"));
+
+    }
+}
+
